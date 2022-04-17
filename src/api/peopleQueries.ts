ddfcb92cd27c6b2d8a9e759ac0df50f6292ac288
Peter Lazarov <peter.lazarov94@gmail.com>
2022-04-17 19:19:17 +0300

    inifite query

--- src/api/peopleQueries.ts
+++ src/api/peopleQueries.ts
@@ -1,5 +1,5 @@
 /* eslint-disable indent */
-import { useInfiniteQuery, useQuery, UseQueryOptions } from 'vue-query'
+import { useInfiniteQuery } from 'vue-query'
 
 const defaultUrl = 'https://swapi.dev/api/people'
 
@@ -19,18 +19,24 @@ interface FindResult {
 interface PeopleFetchParams {
   pageParam?: string
 }
+
 // eslint-disable-next-line arrow-body-style
 const fetchPeople = ({ pageParam = defaultUrl }: PeopleFetchParams): Promise<FindResult> => {
   return fetch(pageParam).then((response) => response.json())
 }
 
-// const peopleFindQuery = async ({ url = defaultUrl }: PeopleFindParams): Promise<FindResult> => fetch(url)
-//   .then((response) => response.json())
-
-// export const usePeopleFind = (params?: PeopleFindParams, options?: UseQueryOptions<FindResult>) => useQuery<FindResult>('peopleFind', () => peopleFindQuery(params), options)
-
-export const usePeopleFindInifite = () =>
+export const usePeopleFindInifite = () => {
 // eslint-disable-next-line implicit-arrow-linebreak
-useInfiniteQuery<FindResult>('peopleFind', fetchPeople, {
-  getNextPageParam: (prevPage) => prevPage.next,
-})
+  const result = useInfiniteQuery<FindResult>('peopleFind', fetchPeople, {
+    getNextPageParam(prevPage) {
+      return prevPage.next
+    },
+    onSuccess(res) {
+      const lastPage = res.pages.at(-1)
+      if (lastPage && lastPage.next) {
+        result.fetchNextPage.value()
+      }
+    }
+  })
+  return result
+}
